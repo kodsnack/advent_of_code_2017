8989e8193dd9e3828489d8bc145fcca8e8288806
Johan Andersson <nilsjohanandersson@gmail.com>
2017-11-21 23:32:51 +0100

    Day 02, part 2

--- day02.lisp
+++ day02.lisp
@@ -10,9 +10,17 @@
 (defun pos (row col)
   (list row col))
 
+(defun keypad-clamp (pos)
+  (pos (clamp (first pos) 0 (1- (array-dimension *keypad* 0)))
+       (clamp (second pos) 0 (1- (array-dimension *keypad* 1)))))
+
 (defun add (x y)
-  (pos (clamp (+ (first x) (first y)) 0 2)
-       (clamp (+ (second x) (second y)) 0 2)))
+  (assert (button x))
+  (let ((sum (keypad-clamp (pos (+ (first x) (first y))
+                                (+ (second x) (second y))))))
+    (if (button sum)
+        sum
+        x)))
 
 (defun move (c)
   (ecase c
@@ -49,3 +57,11 @@
 
 (defun part1 (input)
   (code (input-from-string input)))
+
+(defun part2 (input)
+  (let ((*keypad* (make-array '(5 5) :initial-contents '((nil nil  1 nil nil)
+                                                         (nil  2   3  4  nil)
+                                                         ( 5   6   7  8   9 )
+                                                         (nil :A  :B :C  nil)
+                                                         (nil nil :D nil nil)))))
+    (part1 input)))
--- tests.lisp
+++ tests.lisp
@@ -10,4 +10,5 @@
   (is (= 115 (aoc.day01::part2 (input-for-day 1)))))
 
 (deftest day02 ()
-  (is (equal '(4 7 9 7 8) (aoc.day02::part1 (input-for-day 2)))))
+  (is (equal '(4 7 9 7 8) (aoc.day02::part1 (input-for-day 2))))
+  (is (equal '(6 5 9 :A :D) (aoc.day02::part2 (input-for-day 2)))))
