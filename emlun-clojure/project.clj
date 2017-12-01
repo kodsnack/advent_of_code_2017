2d95e0c7cef6e3ff024a85c46ffef298d31b9fd5
Emil Lundberg <emil@emlun.se>
2017-12-01 21:50:43 +0100

    Add Clojure solution for day01

---
+++ emlun-clojure/project.clj
@@ -0,0 +1,9 @@
+(defproject adventofcode-2017-clojure "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU General Public License, version 3 or higher"
+            :url "https://www.gnu.org/licenses/gpl.txt"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot adventofcode-2017.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
