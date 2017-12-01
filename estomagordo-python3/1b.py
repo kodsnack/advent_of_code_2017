ec930bc15a338d6fec3262021cc22c177e168519
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-12-01 06:24:51 +0100

    estomagordo dag 1

---
+++ estomagordo-python3/1b.py
@@ -0,0 +1,7 @@
+def solver(numstring):
+    l = len(numstring)
+    return sum(int(numstring[x]) if numstring[x] == numstring[(x + l//2) % l] else 0 for x in range(l))
+
+with open('input_1.txt', 'r') as f:    
+    numstring = f.readline().strip()
+    print(solver(numstring))
