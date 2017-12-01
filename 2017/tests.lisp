374a0d085d9ddc62ae86cec01c5e650cb0869ade
Johan Andersson <nilsjohanandersson@gmail.com>
2017-12-01 22:45:07 +0100

    Day 01

--- 2017/tests.lisp
+++ 2017/tests.lisp
@@ -1,3 +1,7 @@
 ;;;; tests.lisp
 
 (in-package #:aoc2017.tests)
+
+(deftest day01 ()
+  (is (= 1182 (aoc2017.day01::part1 (input-for 2017 1))))
+  (is (= 1152 (aoc2017.day01::part2 (input-for 2017 1)))))
