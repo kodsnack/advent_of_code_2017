6a50781806fab840dc8ad83a88a94fe99a5a92d7
Markus Stenqvist <markus.stenqvist@ezy.se>
2017-12-01 14:40:09 +0100

    Stonetwig - Day 1

---
+++ stonetwig-csharp/AdventOfCode17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayToClass = new Dictionary<string, AdventSetup>();
+            string s;
+            var hello = File.ReadAllText(@"hello.txt", Encoding.UTF8);
+            Console.WriteLine("Welcome to stonetwigs Advent of Code solutions for 2017!");
+            Console.WriteLine("When you are done enjoying the image below, please follow the instructions...\n\n");
+            Console.WriteLine(hello);
+
+            dayToClass.Add("1", new Day1());
+
+            do
+            {
+                Console.WriteLine($"Write a number from 1-{dayToClass.Count} for the day you want the solution for (or q for exit)!");
+                s = Console.ReadLine();
+                if (!dayToClass.ContainsKey(s)) continue;
+                var chosenDay = dayToClass[s];
+                chosenDay.Input = chosenDay.GetInput(s);
+                Console.WriteLine($"Final code (day {s}, part 1): {chosenDay.Part1()}");
+                Console.WriteLine($"Final code (day {s}, part 2): {chosenDay.Part2()}");
+            } while (s.ToLower() != "q");
+        }
+    }
+}
