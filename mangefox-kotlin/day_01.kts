13fd03fee459e1d82f3c088d971a72541f3caf3a
mangefox <fox@fojji.se>
2017-12-02 02:06:54 +0100

    Removed reference to own answer

--- mangefox-kotlin/day_01.kts
+++ mangefox-kotlin/day_01.kts
@@ -6,5 +6,5 @@ fun captcha(input: List<Int>, step: Int) =
                      .sum()
 
 val digits = File("day_01_input").readText().map { it-'0' }
-println("solution 1: " + captcha(digits, 1))             // 1393
-println("solution 2: " + captcha(digits, digits.size/2)) // 1292
+println("solution 1: " + captcha(digits, 1))
+println("solution 2: " + captcha(digits, digits.size/2))
