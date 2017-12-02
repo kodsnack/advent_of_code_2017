dc6a53d4b595faf7ea06024f36b76106c414a7cd
Emil Lundberg <emil@emlun.se>
2017-12-02 06:34:28 +0100

    Fix LICENSE files

--- emlun-clojure/project.clj
+++ emlun-clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject adventofcode-2017-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GNU General Public License, version 3 or higher"
-            :url "https://www.gnu.org/licenses/gpl.txt"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot adventofcode-2017.core
   :target-path "target/%s"
