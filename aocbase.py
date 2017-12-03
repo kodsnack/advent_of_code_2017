d8efb969c8b822ac4f3ff1116c2e1a627354290a
Dyr-El <erik.dyrelius@gmail.com>
2017-12-03 17:27:34 +0100

    Added in wrong place

--- aocbase.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-
-def readInput():
-	thisFile = os.path.split(sys.argv[0])[1]
-	thisFileBase = os.path.splitext(thisFile)[0]
-	theTextFile = thisFileBase+'.txt'
-	return open(theTextFile).read().strip()
