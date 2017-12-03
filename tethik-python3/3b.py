eb324b488ac36d99e9fa8444d688425fbd4726cf
Joakim Uddholm <joakim@uddholm.com>
2017-12-03 16:09:46 +0100

    3b solved.

--- tethik-python3/3b.py
+++ tethik-python3/3b.py
@@ -7,11 +7,15 @@ from queue import Queue
 # 351   11   23   25   26
 # 362  747  806--->   ...
 
-def print_map(_map):
+def print_map(_map, i=0):
     for row in _map:
-        print("   ".join(str(i) for i in row))
-
-
+        items = []
+        for cell in row:
+            if not cell:
+                items.append('N')
+            else:
+                items.append(str(cell[i]))
+        print("  ".join(items))
 
 # 1, 3, 5 => 1, 9, 25
 def square_numbers():
@@ -21,96 +25,89 @@ def square_numbers():
         n += 1
         yield n, n ** 2
 
-find_square_number = square_numbers()
-#
-spiral = [
-    [None, None],
-    [1, None]
-]
-next(find_square_number) # skip first
-length_of_side, expansion_point = next(find_square_number)
-q = Queue()
-initial_instructions = [(1,0),(0,-1),(-1,0)]
-for inst in initial_instructions:
-    q.put(inst)
 
-print(expansion_point)
+def neighbourly_sum(spiral, y, x):
+    s = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            try:
+                if spiral[y + i][x + j] and y + i >= 0 and x + j >= 0:
+                    s += spiral[y + i][x + j][1]
+            except: # too lazy to check the bounds
+                pass
+    return s
 
-x, y = 0, 1
-for i in range(2, 100):
+def solve(puzzle):
+    find_square_number = square_numbers()
+    spiral = [
+        [None, None],
+        [(1,1), None]
+    ]
+    next(find_square_number) # skip first
+    length_of_side, expansion_point = next(find_square_number)
+    q = Queue()
+    initial_instructions = [(1,0),(0,-1),(-1,0)]
+    for inst in initial_instructions:
+        q.put(inst)
 
-    instruction = q.get()
-    print(instruction)
-    y += instruction[1]
-    x += instruction[0]
-    spiral[y][x] = i
+    y, x = 1, 0
+    number = 2
+    neighbour_sum = 1
+    while neighbour_sum <= puzzle:
+        instruction = q.get()
 
-    # 1, 9, 25, 36
+        y += instruction[1]
+        x += instruction[0]
 
-    if i == expansion_point:
-        print("Expanding the spiral arrays!")
-        even_cycle = length_of_side % 2 == 0
-        # Expand the map
-        # 4 3
-        # 1 2
-        # >
-        # N 4 3
-        # N 1 2
-        # N N N
-        new_row = [None] * length_of_side
-        if even_cycle:
-            spiral.append(new_row)
-            for row in spiral:
-                row.insert(0, None)
-            q.put((0, 0)) # need to compensate for extra column in front
-            for _ in range(length_of_side):
-                q.put((0, 1))
-            for _ in range(length_of_side):
-                q.put((1, 0))
-        else:
-            spiral.insert(0, new_row)
-            for row in spiral:
-                row.append(None)
-            q.put((1, 1)) # need to compensate for extra row above
-            for _ in range(length_of_side):
-                q.put((0, -1))
-            for _ in range(length_of_side):
-                q.put((-1, 0))
+        neighbour_sum = neighbourly_sum(spiral, y, x)
 
-        # get next value
-        # p = expansion_point
-        length_of_side, expansion_point = next(find_square_number)
+        spiral[y][x] = (number, neighbour_sum)
 
+        # 1, 9, 25, 36
+        if number == expansion_point:
+            print("Expanding the spiral arrays!")
+            even_cycle = length_of_side % 2 == 0
+            # Expand the map
+            # 4 3
+            # 1 2
+            # >
+            # N 4 3
+            # N 1 2
+            # N N N
+            new_row = [None] * length_of_side
+            if even_cycle:
+                spiral.append(new_row)
+                for row in spiral:
+                    row.insert(0, None)
+                q.put((0, 0)) # need to compensate for extra column in front
+                for _ in range(length_of_side):
+                    q.put((0, 1))
+                for _ in range(length_of_side):
+                    q.put((1, 0))
+            else:
+                spiral.insert(0, new_row)
+                for row in spiral:
+                    row.append(None)
+                q.put((1, 1)) # need to compensate for extra row above
+                for _ in range(length_of_side):
+                    q.put((0, -1))
+                for _ in range(length_of_side):
+                    q.put((-1, 0))
 
-    print("#######################################")
-    print_map(spiral)
-    print()
+            # get next value
+            length_of_side, expansion_point = next(find_square_number)
 
-# print_map(spiral)
+        number += 1
 
+        print("###########################")
+        print_map(spiral)
+        print()
+        # print_map(spiral, i=1)
+        # print()
 
+    print(neighbour_sum)
 
-# # 1, 3, 5,
-# def generate_layers():
-#     yield 1, 1
-#     length_of_sides = 1
-#     while True:
-#         length_of_sides += 2
-#         yield length_of_sides, length_of_sides ** 2
-
-# puzzle = int(input())
-
-# for layer, val in enumerate(generate_layers()):
-#     length_of_sides, total = val
-#     print(layer, total)
-#     d = length_of_sides // 2
-#     ld = d + 1
-#     # find closest aligned point (ugly)
-#     points_of_entry = [total - d, total - 3*d, total - 5*d, total - 7*d]
-#     print(points_of_entry)
-#     if total >= puzzle:
-#         steps_to_point = reduce(min, (abs(puzzle - p) for p in points_of_entry))
-#         print(steps_to_point + layer)
-#         break
-
-
+puzzle = int(input())
+solve(puzzle)
