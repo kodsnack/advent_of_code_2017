3ca437bc9fbc71adb261a22ff10e61747df100d0
Jonas Nockert <jonasnockert@gmail.com>
2017-12-03 15:00:25 +0100

    Matlab solution for day 3 (@lemonad)
    In retrospect, I should not have gone the (longer) math route on
    part one as I could not figure out how to make use of it in part
    two. Oh well!

---
+++ lemonad-matlab/day3/day3.m
@@ -0,0 +1,138 @@
+%
+% Day 3, Advent of code 2017 (Jonas Nockert / @lemonad)
+%
+
+assert(fractal_min_dist(1) == 0)
+assert(fractal_min_dist(2) == 1)
+assert(fractal_min_dist(9) == 1)
+assert(fractal_min_dist(10) == 2)
+assert(fractal_min_dist(25) == 2)
+assert(fractal_min_dist(26) == 3)
+assert(fractal_min_dist(49) == 3)
+assert(fractal_min_dist(50) == 4)
+
+assert(taxicab_dist([0 2]) == 2)
+assert(taxicab_dist([3 0]) == 3)
+assert(taxicab_dist([-3 2]) == 5)
+
+%
+% Part one.
+%
+
+assert(solve_part_one(1) == 0)
+assert(solve_part_one(12) == 3)
+assert(solve_part_one(23) == 2)
+assert(solve_part_one(1024) == 31)
+
+fprintf('The taxicab distance is %d\n', solve_part_one(277678));
+
+
+%
+% Part two.
+%
+
+assert(solve_part_two(1) == 2)
+assert(solve_part_two(2) == 4)
+assert(solve_part_two(3) == 4)
+assert(solve_part_two(4) == 5)
+assert(solve_part_two(5) == 10)
+assert(solve_part_two(10) == 11)
+assert(solve_part_two(147) == 304)
+
+fprintf('Next written number is %d\n', solve_part_two(x));
+
+
+%
+% Solutions
+%
+
+assert(taxicab_dist(get_offset(277678)) == 475)
+assert(solve_part_one(277678) == 475)
+assert(solve_part_two(277678) == 279138)
+
+
+%
+% Helpers
+%
+
+function result = solve_part_one(n)
+%SOLVE_PART_ONE Calculate manhattan distance to cell n in fractal memory.
+    result = taxicab_dist(get_offset(n));
+end
+
+function result = solve_part_two(n)
+%SOLVE_PART_TWO Find larger number than `n` in fractal memory sum.
+    min_dist = fractal_min_dist(n);
+
+    % Note addition of 4 extra rings around matrix to avoid
+    % out of bounds (for small n's) when getting the neighborhood
+    % of a cell. For larger n, we should really decrease
+    % side lengths to avoid ridiculously large matrices.
+    side_len = min_dist * 2 + 1 + 4;
+
+    M = zeros(side_len);
+    center = floor(side_len / 2) + 1;
+    M(center, center) = 1;
+    
+    % For n=1, we need to loop to element 3, so add 2.
+    for i = 2:n+2
+        offset = get_offset(i);
+        cx = center + offset(1);
+        cy = center + offset(2);
+        % Sum neighborhood of cell.
+        V = M(cy - 1:cy+ 1, cx - 1:cx + 1);
+        s = sum(sum(V));
+        M(cy, cx) = s;
+        if (s > n)
+            result = s;
+            break
+        end
+    end
+end
+
+function delta = get_offset(n)
+%SOLVE Calculate manhattan distance to cell n in fractal memory.
+
+    % Which "ring" is `n` in? (ring 0 is the innermost, the "1").
+    min_dist = fractal_min_dist(n);
+
+    % Which is the smallest number in the ring?
+    min_n = (max(0, min_dist * 2 - 1))^2 + 1;
+
+    % Distances within a ring follows a repeating pattern, e.g.
+    % [5 4 3 4 5 6 ...] for ring 2 (numbers 26-49), with
+    % corresponding x/y offsets from center following a similar
+    % pattern: [2,1,0,-1,-2,-3], with the sign flipped for the
+    % left and bottom edges, which is why we separate delta
+    % calculations for each side.
+    seq_delta = [min_dist-1:-1:1, 0:-1:-min_dist]';
+    N = length(seq_delta);
+
+    side = floor((n - min_n) / N);
+    side_offset = mod(n - min_n, N) + 1;
+    if side == 0 || side == 2
+        delta = [min_dist, seq_delta(side_offset)];
+    elseif side == 1 || side == 3
+        delta = [seq_delta(side_offset), -min_dist];
+    end
+    
+    % Flip signs for left and bottom sides as we go from
+    % top-to-bottom and left-to-right here.
+    if side >= 2
+        delta = -delta;
+    end
+end
+
+function m = fractal_min_dist(n)
+%FRACTAL_MIN_DIST Gets minimum distance to element `n` in fractal memory.
+    c = ceil(sqrt(n));
+    if mod(c, 2) == 0
+        c = c + 1;
+    end
+    m = floor(c / 2);
+end
+ 
+function d = taxicab_dist(X)
+% TAXICAB_DIST Get taxicab distance of coordinate X.
+    d = abs(X(1)) + abs(X(2));
+end
