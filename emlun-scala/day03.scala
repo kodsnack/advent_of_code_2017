becd06cd08a358c92092843562de0746238dbb90
Emil Lundberg <emil@emlun.se>
2017-12-03 21:45:14 +0100

    Clean up day03 solution

--- emlun-scala/day03.scala
+++ emlun-scala/day03.scala
@@ -1,90 +1,69 @@
 object Main extends App {
+  val input = io.Source.stdin.getLines.toSeq.head.trim.toInt
 
-  val input = 265149
-
-  def straightRight(n: Int): Int =
-    if (n == 0)
+  def straightRight(level: Int): Int =
+    if (level == 0)
       1
     else
-      straightRight(n - 1) + (n-1)*8 + 1
+      straightRight(level - 1) + (level - 1) * 8 + 1
 
-  def btmRight(n: Int): Int = straightRight(n) + n*7
-
-  for { i <- (0 to 25) } {
-    println(s"${i} ${straightRight(i)}")
-  }
-  println(straightRight(1024))
-
-  for { i <- (0 to 25) } {
-    println(s"${i} ${btmRight(i)}")
-  }
-  println(btmRight(1024))
+  def btmRight(level: Int): Int = straightRight(level) + level * 7
 
   def dist(i: Int) = {
-    val n = ((Stream from 0)
-      .find { (m: Int) =>
-        i <= btmRight(m)
+    val level = ((Stream from 0)
+      .find { (l: Int) =>
+        i <= btmRight(l)
       }
       .get
     )
 
-    val indexInCircle = i - btmRight(Math.max(0, n - 1)) - n
-    val modulus = Math.max(1, 2*n)
+    val indexInCircle = i - btmRight(Math.max(0, level - 1)) - level
+    val modulus = Math.max(1, 2 * level)
 
-    // (n, indexInCircle, modulus, Math.abs(indexInCircle % modulus))
-    val d = n + Math.abs(indexInCircle % modulus)
-    s"n ${n}, i ${indexInCircle}, m ${modulus}, a ${Math.abs(indexInCircle % modulus)}, d ${d}"
+    level + Math.abs(indexInCircle % modulus)
   }
 
-  for { i <- (0 to 30) } {
-    println(s"${i} ${dist(i)}")
-  }
-  println(dist(1024))
-  println(dist(input))
-
-  def shell(n: Int): List[Int] = {
-    if (n <= 2)
+  def shell(level: Int): List[Int] = {
+    if (level <= 2)
       List(1, 2, 4, 5, 10, 11, 23, 25)
     else {
-      val prev = shell(n - 1)
+      val prev = shell(level - 1)
       val length = prev.length + 8
-
-      val m = 2*(n - 1) - 1
-      println(s"m ${m}")
+      val m = 2 * (level - 1) - 1
 
       def value(i: Int): Int = i match {
-        case 0                         => prev.last + prev.head
-        case 1                         => prev.last + prev.head + prev(1) + value(0)
-        case i if i < m-1              => prev(i - 2) + prev(i - 1) + prev(i) + value(i - 1)
-        case i if i == m-1             => prev(i - 2) + prev(i - 1) + value(i - 1)
-        case i if i == m               => prev(i - 2) + value(i - 1)
-        case i if i == m+1             => prev(i - 3) + prev(i - 2) + value(i - 2) + value(i - 1)
-        case i if i < m+m              => prev(i - 4) + prev(i - 3) + prev(i - 2) + value(i - 1)
-        case i if i == m+m             => prev(i - 4) + prev(i - 3) + value(i - 1)
-        case i if i == m+m+1           => prev(i - 4) + value(i - 1)
-        case i if i == m+m+1+1         => prev(i - 5) + prev(i - 4) + value(i - 1) + value(i - 2)
-        case i if i < m+m+1+m          => prev(i - 6) + prev(i - 5) + prev(i - 4) + value(i - 1)
-        case i if i == m+m+1+m         => prev(i - 6) + prev(i - 5) + value(i - 1)
-        case i if i == m+m+1+m+1       => prev(i - 6) + value(i - 1)
-        case i if i == m+m+1+m+1+1     => prev(i - 7) + prev(i - 6) + value(i - 1) + value(i - 2)
-        case i if i < m+m+1+m+1+m      => prev(i - 8) + prev(i - 7) + prev(i - 6) + value(i - 1)
-        case i if i == m+m+1+m+1+m     => prev(i - 8) + prev(i - 7) + value(i - 1) + value(0)
-        case i if i == m+m+1+m+1+m+1   => prev(i - 8) + value(i - 1) + value(0)
+        case 0                       =>                prev.last   + prev(i)
+        case 1                       => prev.last    + prev(i - 1) + prev(i)     + value(i - 1)
+        case i if i < m-1            => prev(i - 2)  + prev(i - 1) + prev(i)     + value(i - 1)
+        case i if i == m-1           => prev(i - 2)  + prev(i - 1)               + value(i - 1)
+        case i if i == m             => prev(i - 2)                              + value(i - 1)
+        case i if i == m+1           => value(i - 2) + prev(i - 2) + prev(i - 3) + value(i - 1)
+
+        case i if i < m+m            => prev(i - 4)  + prev(i - 3) + prev(i - 2) + value(i - 1)
+        case i if i == m+m           => prev(i - 4)  + prev(i - 3)               + value(i - 1)
+        case i if i == m+m+1         => prev(i - 4)                              + value(i - 1)
+        case i if i == m+m+1+1       => value(i - 2) + prev(i - 4) + prev(i - 5) + value(i - 1)
+
+        case i if i < m+m+1+m        => prev(i - 6)  + prev(i - 5) + prev(i - 4) + value(i - 1)
+        case i if i == m+m+1+m       => prev(i - 6)  + prev(i - 5)               + value(i - 1)
+        case i if i == m+m+1+m+1     => prev(i - 6)                              + value(i - 1)
+        case i if i == m+m+1+m+1+1   => value(i - 2) + prev(i - 6) + prev(i - 7) + value(i - 1)
+
+        case i if i < m+m+1+m+1+m    => prev(i - 8) + prev(i - 7) + prev(i - 6) + value(i - 1)
+        case i if i == m+m+1+m+1+m   => prev(i - 8) + prev(i - 7) + value(0)    + value(i - 1)
+        case i if i == m+m+1+m+1+m+1 => prev(i - 8) + value(0)                  + value(i - 1)
       }
 
-      val sh = (0 until length).toList map value
-      println(s"${n} ${sh}")
-      sh
+      (0 until length).toList map value
     }
   }
 
+  def solveB(input: Int): Int =
+    (Stream from 0)
+      .flatMap(shell)
+      .find(_ > input)
+      .get
 
-  println(shell(2))
-  println(shell(3))
-
-  println((Stream from 0) flatMap shell find { i => i > input })
-
-  // val (a, b) = lineChecksums.toSeq.unzip
-  // println(s"A: ${a.sum}")
-  // println(s"B: ${b.sum}")
+  println(s"A: ${dist(input)}")
+  println(s"B: ${solveB(input)}")
 }
