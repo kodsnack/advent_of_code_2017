975c253f8e997680c9a469e65f9c3acc3b4ef904
Emil Lundberg <emil@emlun.se>
2017-12-04 06:05:31 +0100

    Solve day04a

---
+++ emlun-scala/day04.scala
@@ -0,0 +1,17 @@
+object Main extends App {
+
+  val words: List[List[String]] = (for {
+    line <- io.Source.stdin.getLines
+    trimmed = line.trim
+  } yield trimmed.split(raw"\s+").toList).toList
+
+  def isValid(passphrase: List[String]): Boolean =
+    passphrase.toSet.size == passphrase.size
+
+  val numValid = words.filter(isValid).size
+
+  println(numValid)
+
+  // println(s"A: ${dist(input)}")
+  // println(s"B: ${solveB(input)}")
+}
