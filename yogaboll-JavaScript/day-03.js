d28a7b80f677707c62786490d61c95d9e2c3bb4d
yogaboll <markus.s.englund@gmail.com>
2017-12-04 13:38:48 +0700

    day 3b

--- yogaboll-JavaScript/day-03.js
+++ yogaboll-JavaScript/day-03.js
@@ -14,7 +14,8 @@ Steps from middle to number
 */
 
 const input = 312051;
-// const input = 49;
+
+// Day 3a
 const ring = Math.round(Math.sqrt(input - 1) / 2);
 const semanticallyMeaninglessVariable =
   (input + ring - (2 * ring - 1) ** 2) % (2 * ring);
@@ -24,4 +25,42 @@ const sideSteps = Math.min(
 );
 
 const totalSteps = ring + sideSteps;
-console.log(sideSteps, ring, totalSteps);
+console.log(totalSteps);
+
+// Day 3b
+
+const squares = [{ x: 0, y: 0, value: 1 }];
+const directions = ["down", "right", "up", "left"];
+for (let i = 1; squares[squares.length - 1].value < input; i += 1) {
+  const direction = directions[i % 4];
+  for (
+    let j = 0;
+    j < Math.round(i / 2) && squares[squares.length - 1].value < input;
+    j += 1
+  ) {
+    let x;
+    let y;
+    if (direction === "down") {
+      x = squares[squares.length - 1].x;
+      y = squares[squares.length - 1].y - 1;
+    } else if (direction === "right") {
+      x = squares[squares.length - 1].x + 1;
+      y = squares[squares.length - 1].y;
+    } else if (direction === "up") {
+      x = squares[squares.length - 1].x;
+      y = squares[squares.length - 1].y + 1;
+    } else {
+      x = squares[squares.length - 1].x - 1;
+      y = squares[squares.length - 1].y;
+    }
+    const adjacentSquares = squares.filter(
+      square => Math.abs(square.x - x) < 2 && Math.abs(square.y - y) < 2
+    );
+    const value = adjacentSquares.reduce(
+      (acc, square) => acc + square.value,
+      0
+    );
+    squares.push({ x, y, value });
+    console.log("value: ", value);
+  }
+}
