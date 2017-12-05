7dd0ac9a264c0e44390a5d18357dbc5718b9d831
Mats Nordgren <telcopro@gmail.com>
2017-12-05 21:11:02 +0100

    3b ready.

--- telcopro-go/3/3.go
+++ telcopro-go/3/3.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-
-const debug = 0
-
 type Turtle struct {
 	x, y, dx, dy int
 	val          [1000][1000]int
@@ -12,60 +8,50 @@ type Turtle struct {
 func forward(t *Turtle, newVal int) {
 	t.x = t.x + t.dx
 	t.y = t.y + t.dy
-	if debug == 2 {
-		fmt.Printf("Moving forward to coordinates (%d, %d) and setting value to %d\n", t.x, t.y, newVal)
-	}
 	t.val[t.x+500][t.y+500] = newVal
 	return
 }
 
-func turnLeft(t *Turtle) {
+func forwardNoset(t *Turtle) {
+	t.x = t.x + t.dx
+	t.y = t.y + t.dy
+	return
+}
 
+func turnLeft(t *Turtle) {
 	if t.dx == 1 && t.dy == 0 {
-		if debug == 1 {
-			fmt.Println("Heading was east, new heading north")
-		}
 		t.dx, t.dy = 0, 1
 	} else if t.dx == 0 && t.dy == 1 {
-		if debug == 1 {
-			fmt.Println("Heading was north, new heading west")
-		}
 		t.dx, t.dy = -1, 0
 	} else if t.dx == -1 && t.dy == 0 {
-		if debug == 1 {
-			fmt.Println("Heading was west, new heading south")
-		}
 		t.dx, t.dy = 0, -1
 	} else if t.dx == 0 && t.dy == -1 {
-		if debug == 1 {
-			fmt.Println("Heading was south, new heading east")
-		}
 		t.dx, t.dy = 1, 0
 	}
 }
 
 func lookLeft(t *Turtle) int {
 	if t.dx == 1 && t.dy == 0 {
-		if debug == 1 {
-			fmt.Printf("Heading was east, peeking north and finding %d\n", t.val[t.x+500][t.y+501])
-		}
 		return t.val[t.x+500][t.y+501]
 	} else if t.dx == 0 && t.dy == 1 {
-		if debug == 1 {
-			fmt.Printf("Heading was north, peeking west and finding %d\n", t.val[t.x+499][t.y+500])
-		}
 		return t.val[t.x+499][t.y+500]
 	} else if t.dx == -1 && t.dy == 0 {
-		if debug == 1 {
-			fmt.Printf("Heading was west, peeking south and finding %d\n", t.val[t.x+500][t.y+499])
-		}
 		return t.val[t.x+500][t.y+499]
 	} else if t.dx == 0 && t.dy == -1 {
-		if debug == 1 {
-			fmt.Printf("Heading was south, peeking east and finding %d\n", t.val[t.x+501][t.y+500])
-		}
 		return t.val[t.x+501][t.y+500]
 	} else {
 		return 0
 	}
+}
+
+func sqSum(t *Turtle) int {
+
+	acc := 0
+
+	for a := -1; a <= 1; a++ {
+		for b := -1; b <= 1; b++ {
+			acc += t.val[t.x+500+a][t.y+500+b]
+		}
+	}
+	return acc
 }
