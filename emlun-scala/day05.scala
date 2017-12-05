6267cf53524abac890e6919db1603973a39be919
Emil Lundberg <emil@emlun.se>
2017-12-05 06:27:06 +0100

    Solve day05b

--- emlun-scala/day05.scala
+++ emlun-scala/day05.scala
@@ -1,27 +1,35 @@
 object Main extends App {
-  val jumps: List[Int] = (for {
+  val jumps: Array[Int] = (for {
     line <- io.Source.stdin.getLines
-  } yield line.trim.toInt).toList
+  } yield line.trim.toInt).toArray
 
-  def jump(eip: Int, jumps: List[Int]): (Int, List[Int]) =
-    (eip + jumps(eip), jumps.updated(eip, jumps(eip) + 1))
+  def jump(eip: Int, jumps: Array[Int]): (Int, Int) =
+    (eip + jumps(eip), jumps(eip) match {
+      case i if i >= 3 => jumps(eip) - 1
+      case i => jumps(eip) + 1
+    })
 
-  def solveA(input: List[Int]): Int = {
+  def solveA(input: Array[Int]): Int = {
     var eip = 0
     var jumps = input
     var numJumps = 0
 
-    while (eip < jumps.size) {
+    while (eip < jumps.size && eip >= 0) {
+      if (numJumps % 100000 == 0) {
+        println(numJumps, eip)
+      }
+      // println(jumps.toList)
+
       val pair = jump(eip, jumps)
+      jumps.update(eip, pair._2)
       eip = pair._1
-      jumps = pair._2
       numJumps += 1
     }
 
     numJumps
   }
 
-  // def solveB(input: List[String]): Int =
+  // def solveB(input: Array[String]): Int =
     // input.filter({ p => p.map(_.toSet).toSet.size == p.size }).size
 
   println(s"A: ${solveA(jumps)}")
