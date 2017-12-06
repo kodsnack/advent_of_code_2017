e0bb1d57f48be01f30b8f3d7248c15b9dc99ff0e
Emil Lundberg <emil@emlun.se>
2017-12-06 06:18:36 +0100

    Solve day06a

---
+++ emlun-scala/day06.scala
@@ -0,0 +1,34 @@
+object Main extends App {
+  val banks: List[Int] = (for {
+    line <- io.Source.stdin.getLines
+    bank <- line.trim.split(raw"\s+")
+  } yield bank.toInt).toList
+
+  def findMax(banks: List[Int]): (Int, Int) = {
+    val value = banks.max
+    val index = banks.indexOf(value)
+    (index, value)
+  }
+
+  def redistribute(banks: List[Int], index: Int, blocks: Int): List[Int] =
+    if (blocks == 0)
+      banks
+    else
+      redistribute(banks.updated(index, banks(index) + 1), (index + 1) % banks.length, blocks - 1)
+
+  def step(banks: List[Int]): List[Int] = {
+    val (i, m) = findMax(banks)
+    redistribute(banks.updated(i, 0), (i + 1) % banks.length, m)
+  }
+
+  def indexOfFirstRecurrence(history: Set[List[Int]], present: List[Int]): Int =
+    if (history contains present)
+      history.size
+    else
+      indexOfFirstRecurrence(history + present, step(present))
+
+  println(s"A: ${banks}")
+  println(s"A: ${step(banks)}")
+  println(s"A: ${indexOfFirstRecurrence(Set.empty, banks)}")
+}
+
