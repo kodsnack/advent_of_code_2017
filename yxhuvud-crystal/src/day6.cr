af5ab998d825f5624795b7dd754067620e75e1a0
Linus Sellberg <sellberg@gmail.com>
2017-12-06 18:05:58 +0100

    Remove cruft.

--- yxhuvud-crystal/src/day6.cr
+++ yxhuvud-crystal/src/day6.cr
@@ -1,39 +1,20 @@
 INPUT = "10     3       15      10      5       15      5       15      9       2       5       8       5       2       3       6"
 
 start = INPUT.split.map &.to_i
-
-class Pos
-  include Comparable(self)
-
-  property value : Int32
-  property index : Int32
-
-  def initialize(@value, @index); end
-
-  def <=>(other)
-    return index <=> other.index if self.value == other.value
-    other.value <=> value
-  end
-end
-
-banks = start.map_with_index { |v, i| Pos.new(v, i) }
-
-seen = {banks.map(&.value) => 0}
-
+banks = start.map_with_index { |v, i| [v, -i] }
+seen = {banks.map(&.[0]) => 0}
 queue = banks.sort
 
 (1..Int32::MAX).each do |n|
-  val = queue.shift
-  value, val.value = val.value, 0
-  value.times do |i|
-    banks[(val.index + i + 1) % banks.size].value += 1
-  end
-  queue.push val
+  val = queue.pop
+  value, val[0] = val[0], 0
+  value.times { |i| banks[(-val[1] + i + 1) % banks.size][0] += 1}
+  queue.unshift val
   # eww, it would be nice with a pqueue in stdlib. But bother doing a
   # more efficient solution when the program takes 0.007 seconds to
   # run..
   queue.sort!
-  step = banks.map(&.value)
+  step = banks.map(&.[0])
   if seen[step]?
     puts "part 1"
     puts n
