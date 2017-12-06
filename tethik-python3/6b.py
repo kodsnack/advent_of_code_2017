e2da3c2105b4c65f4c0884d7b6c6fc91f2f2e39c
Joakim Uddholm <joakim@uddholm.com>
2017-12-06 09:31:00 +0100

    Dag 6 löst.

---
+++ tethik-python3/6b.py
@@ -0,0 +1,31 @@
+memory_banks = [int(s) for s in input().strip().split()]
+state = "-".join(str(i) for i in memory_banks)
+visited = dict()
+
+
+inp = 0
+steps = 0
+while state not in visited:
+    visited[state] = steps
+
+    # O(n) find max
+    _max = memory_banks[0]
+    start_index = 0
+    for i, bank in enumerate(memory_banks):
+        if bank > _max:
+            _max = bank
+            start_index = i
+
+    memory_banks[start_index] = 0
+
+    # distribute.
+    while _max > 0:
+        start_index = (start_index + 1) % len(memory_banks)
+        memory_banks[start_index] += 1
+        _max -= 1
+
+    steps += 1
+    state = "-".join(str(i) for i in memory_banks)
+    print(steps, state)
+
+print(steps - visited[state])
