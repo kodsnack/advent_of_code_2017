88c3b785a83b7b3ff9f16dbb00a066a575dc0f0f
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-12-08 06:38:00 +0100

    estomagordo 8 december

---
+++ estomagordo-python3/8b.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+def solve(instructions):
+    highest = 0
+    registers = defaultdict(int)
+
+    for instruction in instructions:
+        a = instruction[0]
+        b = instruction[4]
+        bval = registers[b]
+        achange = int(instruction[2])
+        bcomp = int(instruction[-1])
+        operator = instruction[-2]
+        sign = 1 if instruction[1] == 'inc' else -1
+
+        succeeds = True
+
+        if operator == '==':
+            succeeds = bval == bcomp
+        elif operator == '>':
+            succeeds = bval > bcomp
+        elif operator == '>=':
+            succeeds = bval >= bcomp
+        elif operator == '<':
+            succeeds = bval < bcomp
+        elif operator == '<=':
+            succeeds = bval <= bcomp
+        elif operator == '!=':
+            succeeds = bval != bcomp
+
+        if succeeds:
+            registers[a] += sign * achange
+            highest = max(highest, registers[a])
+
+    return highest
+
+t = ['b inc 5 if a > 1', 'a inc 1 if b < 5', 'c dec -10 if a >= 1', 'c inc -20 if c == 10']
+ 
+print(solve([line.split() for line in t]))   
+
+with open('input_8.txt', 'r') as f:
+    instructions = [line.split() for line in f.readlines()]
+    print(solve(instructions))
