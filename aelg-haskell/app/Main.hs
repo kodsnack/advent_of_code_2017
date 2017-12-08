d0011ef77d3b6f276cdb313414d83f00265f88a0
Linus Mellberg <linus.mellberg@gmail.com>
2017-12-08 08:26:27 +0100

    Day 8

--- aelg-haskell/app/Main.hs
+++ aelg-haskell/app/Main.hs
@@ -16,8 +16,19 @@ import qualified Four
 import qualified Five
 import qualified Six
 import qualified Seven
+import qualified Eight
 
-solved = M.fromList [(1, One.solve), (2, Two.solve), (3, Three.solve), (4, Four.solve), (5, Five.solve), (6, Six.solve), (7, Seven.solve)]
+solved =
+  M.fromList
+    [ (1, One.solve)
+    , (2, Two.solve)
+    , (3, Three.solve)
+    , (4, Four.solve)
+    , (5, Five.solve)
+    , (6, Six.solve)
+    , (7, Seven.solve)
+    , (8, Eight.solve)
+    ]
 
 getSolution x = M.findWithDefault notImplemented x solved
 
