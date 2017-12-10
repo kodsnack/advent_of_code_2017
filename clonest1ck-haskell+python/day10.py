0af6ee553f18db717c2b6088bd562271bd290c44
Anton Bengtsson <anton@antonbengtsson.net>
2017-12-10 10:16:59 +0100

    Day 10

--- clonest1ck-haskell+python/day10.py
+++ clonest1ck-haskell+python/day10.py
@@ -13,18 +13,29 @@ def reverseList(l, start, end):
         start = (start + 1) % rng
 
 f = open("in10.txt", 'r')
-f = [int(x) for x in f.readline().split("\n")[0].split(",")]
+f = [ord(x) for x in f.readline().split("\n")[0]] + [17, 31, 73, 47, 23]
 
 hashlist = range(rng)
 pos = 0
 skip = 0
+for i in range(64):
+    for length in f:
+        if(length != 0):
+            [start, end] = [pos, (pos + length - 1) % rng]
+            hashlist = reverseList(hashlist, start, end)
+        pos = (pos + length + skip) % rng
+        skip += 1
 
-for length in f:
-    if(length != 0):
-        [start, end] = [pos, (pos + length - 1) % rng]
-        hashlist = reverseList(hashlist, start, end)
-    pos = (pos + length + skip) % rng
-    skip += 1
-    print hashlist
+res = ""
+for i in range(16):
+    hexsum = 0
+    for j in range(16):
+        index = i * 16 + j
+        hexsum ^= hashlist[index]
 
-print "Task 1: " + str(hashlist[0] * hashlist[1])
+    hexstr = hex(hexsum).split("x")[1]
+    if(len(hexstr) < 2):
+        hexstr = "0" + hexstr
+    res += hexstr
+
+print res
