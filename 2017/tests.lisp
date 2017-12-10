f21c00315e802fae79c409a6df79bc75d267d17e
Johan Andersson <nilsjohanandersson@gmail.com>
2017-12-10 16:03:40 +0100

    Day 10, part 2

--- 2017/tests.lisp
+++ 2017/tests.lisp
@@ -35,4 +35,5 @@
   (is (= 6622 (aoc2017.day09::part2 (input-for 2017 9)))))
 
 (deftest day10 ()
-  (is (= 19591 (aoc2017.day10::part1 (input-for 2017 10)))))
+  (is (= 19591 (aoc2017.day10::part1 (input-for 2017 10))))
+  (is (string= "62e2204d2ca4f4924f6e7a80f1288786" (aoc2017.day10::part2 (input-for 2017 10)))))
