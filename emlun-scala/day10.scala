0e34b8508d393b785be4da755ef174ab91afac97
Emil Lundberg <emil@emlun.se>
2017-12-10 06:41:07 +0100

    Solve day10b

--- emlun-scala/day10.scala
+++ emlun-scala/day10.scala
@@ -2,29 +2,19 @@ object Main extends App {
 
   val nums = (for {
     line <- io.Source.stdin.getLines
-    num <- line.trim.split(",")
-  } yield num.toInt).toList
+    char: Char <- line
+  } yield char.toInt).toList
 
   def process(lengths: List[Int], numbers: List[Int] = (0 to 255).toList, pos: Int = 0, skip: Int = 0): List[Int] = lengths match {
-    case Nil => {
-      println(numbers)
-      numbers
-    }
+    case Nil => numbers
     case length :: restLengths => {
-      println(numbers, length, pos, skip)
       val op1 = (numbers ++ numbers).splitAt(pos) match {
         case (newHead, back) => back.splitAt(length) match {
           case (front, newTail) => {
-            println(s"newHead: ${newHead}")
-            println(s"back: ${back}")
-            println(s"front: ${front}")
-            println(s"newTail: ${newTail}")
-
             val wrapLength =
               if (pos + length >= numbers.size)
                 (pos + length) % numbers.size
               else 0
-
             val selection = front.reverse
 
             (selection.drop(length - wrapLength).take(wrapLength)
@@ -39,8 +29,6 @@ object Main extends App {
       val nextPos = (pos + length + skip) % numbers.size
       val nextSkip = skip + 1
 
-      println()
-
       process(restLengths, op1, nextPos, nextSkip)
     }
   }
@@ -49,5 +37,19 @@ object Main extends App {
     case a :: b :: tail => a * b
   }
 
-  println(s"A: ${solveA(nums)}")
+  def solveB(bytes: List[Int]): String = {
+    val padded = bytes ++ List(17, 31, 73, 47, 23)
+    val bytesForAllRounds: List[Int] = (1 to 64).toList flatMap { i => padded }
+    val sparseHash: List[Int] = process(bytesForAllRounds)
+    val denseHash: List[Int] = sparseHash
+      .grouped(16)
+      .map { block =>
+        block.reduce({ _ ^ _ })
+      }
+      .toList
+
+    denseHash map { d => f"${d}%02x" } mkString ""
+  }
+
+  println(s"B: ${solveB(nums)}")
 }
