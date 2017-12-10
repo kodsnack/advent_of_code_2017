f81d8ccfbacd4b1dbd613271a09108344826404b
Erik Dyrelius <erik.dyrelius@gmail.com>
2017-12-10 07:41:08 +0100

    Day 10 some kind of solution

--- erikdyrelius-python3/aocbase.py
+++ erikdyrelius-python3/aocbase.py
@@ -5,5 +5,6 @@ import os
 def readInput():
 	thisFile = os.path.split(sys.argv[0])[1]
 	thisFileBase = os.path.splitext(thisFile)[0]
+	thisFileBase = thisFileBase.split("_")[0]
 	theTextFile = thisFileBase+'.txt'
 	return open(theTextFile).read().strip()
