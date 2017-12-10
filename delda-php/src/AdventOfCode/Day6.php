a26fed1dda1b197823e37d175c48bc6036804a48
Davide <delda@virgilio.it>
2017-12-10 01:10:53 +0100

    Advent of code: day 6

---
+++ delda-php/src/AdventOfCode/Day6.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace src\AdventOfCode;
+
+use SebastianBergmann\CodeCoverage\Report\PHP;
+
+class Day6 extends AbstractAdventOfCode
+{
+    protected $class = 6;
+
+    public function firstPart(): string
+    {
+        return sizeof($this->memoryReallocation()) - 1;
+    }
+
+    public function secondPart(): string
+    {
+        $solution = $this->memoryReallocation();
+        return (sizeof($solution) - 1) - current(array_keys($solution, end($solution)));
+    }
+
+    private function memoryReallocation(): array
+    {
+        $memoryBanks = explode("\t", $this->input);
+        $sizeOfMemoryBanks = sizeof($memoryBanks);
+        $savedStates = [];
+
+        while(!in_array($memoryBanks, $savedStates)) {
+            $savedStates[] = $memoryBanks;
+            $bloks = max($memoryBanks);
+            $bankIdx = current(array_keys($memoryBanks, $bloks));
+            $memoryBanks[$bankIdx] = 0;
+            $i = 1;
+            while ($bloks > 0) {
+                $memoryBanks[($bankIdx+$i)%$sizeOfMemoryBanks]++;
+                $bloks--;
+                $i++;
+            }
+        }
+        $savedStates[] = $memoryBanks;
+
+        return $savedStates;
+    }
+}
