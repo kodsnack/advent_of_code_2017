10df54139fabd1ecd9b7b26ac64cc0def9f6a56a
Joakim Uddholm <joakim@uddholm.com>
2017-12-12 00:42:53 +0100

    11a tråkigare men bättre :(

--- tethik-python3/11a.py
+++ tethik-python3/11a.py
@@ -1,56 +1,19 @@
-from collections import Counter
-
 line = input()
+
 conversions = {
-    "ne": (2,1),
+    "ne": (1,1),
     "n": (0,2),
-    "nw": (-2,1),
-    "sw": (-2,-1),
+    "nw": (-1,1),
+    "sw": (-1,-1),
     "s": (0,-2),
-    "se": (2,-1),
+    "se": (1,-1),
 }
 
-parts = sorted(line.split(","))
-
+parts = line.split(",")
 
 pos = (0,0)
 for p in parts:
     v = conversions[p]
     pos = (pos[0] + v[0], pos[1] + v[1])
 
-x,y = pos
-assert x % 2 == 0
-
-new_steps = []
-while x < 0:
-    if y > 0:
-        new_steps.append("se")
-        x += 2
-        y -= 1
-    else:
-        new_steps.append("ne")
-        x += 2
-        y += 1
-
-while x > 0:
-    if y > 0:
-        new_steps.append("sw")
-        x -= 2
-        y -= 1
-    else:
-        new_steps.append("nw")
-        x -= 2
-        y += 1
-
-assert y % 2 == 0
-
-while y > 0:
-    new_steps.append("s")
-    y -= 2
-
-while y < 0:
-    new_steps.append("n")
-    y += 2
-
-print(len(new_steps))
-# print(abs(pos[0]) + abs(round(pos[1])))
+print(abs(pos[0]) + (abs(pos[1]) - abs(pos[0])) // 2)
