d280f75ae613ae9f41e78b4a458c8b250ba5a92c
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-12-11 07:00:36 +0100

    Dec 11

---
+++ estomagordo-python3/11a.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+def distance(x, y):
+    xsteps = abs(x)
+    ysteps = (abs(y) - xsteps) // 2
+    return xsteps + ysteps
+
+def solve(steps):
+    taken = defaultdict(int)
+    x = 0
+    y = 0
+
+    for step in steps:
+        if step == 'nw':
+            x -= 1
+            y += 1            
+        elif step == 'n':
+            y += 2
+        elif step == 'ne':
+            y += 1
+            x += 1
+        elif step == 'sw':
+            y -= 1
+            x -= 1
+        elif step == 's':
+            y -= 2
+        else:
+            y -= 1
+            x += 1
+    
+    return(distance(x, y))
+
+with open('input.txt', 'r') as f:
+    steps = f.read().rstrip().split(',')
+    print(solve(steps))
---
+++ estomagordo-python3/11b.py
@@ -0,0 +1,35 @@
+def distance(x, y):
+    xsteps = abs(x)
+    ysteps = (abs(y) - xsteps) // 2
+    return xsteps + ysteps
+
+def solve(steps):
+    x = 0
+    y = 0
+    longest = 0
+
+    for step in steps:
+        if step == 'nw':
+            x -= 1
+            y += 1            
+        elif step == 'n':
+            y += 2
+        elif step == 'ne':
+            y += 1
+            x += 1
+        elif step == 'sw':
+            y -= 1
+            x -= 1
+        elif step == 's':
+            y -= 2
+        else:
+            y -= 1
+            x += 1
+
+        longest = max(longest, distance(x, y))
+    
+    return(longest)
+
+with open('input.txt', 'r') as f:
+    steps = f.read().rstrip().split(',')
+    print(solve(steps))
