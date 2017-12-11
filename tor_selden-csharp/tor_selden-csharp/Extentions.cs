d0efc4884b2817e5a26d3f50e1fb8a02efefbb6c
torselden <tor.selden@gmail.com>
2017-12-11 17:52:20 +0100

    Extentions

---
+++ tor_selden-csharp/tor_selden-csharp/Extentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tor_selden_csharp
+{
+    public static class Extentions
+    {
+        public static bool HasDuplicates<T>(this IEnumerable<T> list)
+        {
+            var hashset = new HashSet<T>();
+            return list.Any(e => !hashset.Add(e));
+        }
+
+        public static int Weight(this string program)
+        {
+            int weight = int.Parse(program.Split(new[] { '(', ')' }).First());
+            return weight;
+        }
+
+        //public static bool IsBalanced(this string program)
+        //{
+        //    if (!String.IsNullOrEmpty(program))
+        //    {
+
+        //    }
+        //    return true;
+        //}
+    }
+}
