4b06ac226a93481d530c1b87043aa12d2e11ecab
Erik Dyrelius <erik.dyrelius@gmail.com>
2017-12-13 09:02:15 +0100

    More elegant solution

--- erikdyrelius-python3/day13.py
+++ erikdyrelius-python3/day13.py
@@ -9,36 +9,40 @@ ex = """0: 3
 6: 4"""
 
 p = re.compile(r"(\d+): (\d+)")
-fw = list()
-for line in inp.splitlines():
-    m = p.match(line)
-    if m:
-        fw.append((int(m.group(1)), int(m.group(2))))
-    else:
-        print(">>>",line)
-sev = 0
-for d, r in fw:
-    if d % ((r-1)*2) == 0:
-        sev += d*r
 
-print(sev)
+def parseTable(inp):
+    fw = list()
+    for line in inp.splitlines():
+        m = p.match(line)
+        if m:
+            fw.append((int(m.group(1)), (int(m.group(2))-1)*2))
+        else:
+            print(">>>",line)
+    fw.sort(key=lambda x:x[1]*1000+x[0])
+    return fw
 
-def sgd(a, b):
-    if a % b == 0:
-        return a
-    else:
-        return sgd(b, a%b)
-
-de = 1
-while True:
-    safe = True
+def calcSeverity(fw):
+    sev = 0
     for d, r in fw:
-        if (d + de) % ((r-1)*2) == 0:
-            safe = False
-            print(de, d, r)
+        if d % r == 0:
+            sev += d*((r//2)+1)
+    return sev
+
+def findPassage(fw):
+    delay = 0
+    while True:
+        safe = True
+        for d, r in fw:
+            if (d + delay) % r == 0:
+                safe = False
+                break
+        if safe:
             break
-    if safe:
-        break
-    de += 1
-print(de)
+        delay += 1
+    return delay
+
+print("Puzzle 1, example {}".format(calcSeverity(parseTable(ex))))
+print("Puzzle 1, solution {}".format(calcSeverity(parseTable(inp))))
+print("Puzzle 2, example {}".format(findPassage(parseTable(ex))))
+print("Puzzle 2, solution {}".format(findPassage(parseTable(inp))))
 #3875560
