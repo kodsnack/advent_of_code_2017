3ee0915edf332ca11b4eea0da7193191b96694e4
Erik Jansson <erikjansson90@gmail.com>
2017-12-13 09:38:15 +0100

    Finish Day 13

--- meldanya-python3/day13/day13.py
+++ meldanya-python3/day13/day13.py
@@ -1,35 +1,48 @@
 import sys
 
 
-def part1(scanners):
+def _caught(p, scanner):
+    if scanner != 0 and p % (2 * (scanner - 1)) == 0:
+        return True
+    return False
+
+
+def solve(scanners, delay=0, abort=False):
     severity = 0
-    scanner_pos = {s: 0 for s, _ in scanners}
-    scanner_dirs = {s: 1 for s, _ in scanners}
-    stop = scanners[-1][0]
-    print(scanner_pos)
-    for p in range(stop):
-        for d, r in scanners:
-            if scanner_pos[d] == p and p == d:
-                print('caught at depth %d by scanner %d at pos %d' % (
-                    p, d, scanner_pos[d]))
-                severity += d*r
-            scanner_pos[d] += scanner_dirs[d]
-            if scanner_pos[d] >= r or scanner_pos[d] == 0:
-                scanner_dirs[d] *= -1
-    return severity
+    caught = False
+    time = delay
+    for level, scanner in enumerate(scanners):
+        if _caught(time, scanner):
+            severity += level * scanner
+            caught = True
+            if abort:
+                break
+        time += 1
+    return severity, caught
 
 
-def part2():
-    pass
+def part1(scanners):
+    return solve(scanners)[0]
+
+
+def part2(scanners):
+    delay = 0
+    caught = True
+    while caught:
+        delay += 1
+        _, caught = solve(scanners, delay=delay, abort=True)
+    return delay
 
 
 def main():
     lines = [l for l in sys.stdin]
-    scanners = [tuple(int(p.strip()) for p in line.split(':'))
-                for line in lines]
-    print(scanners)
+    n = int(lines[-1].split(':')[0].strip())+1
+    scanners = [0]*n
+    for line in lines:
+        d, r = [int(p.strip()) for p in line.split(':')]
+        scanners[d] = r
     print(part1(scanners))
-    print(part2())
+    print(part2(scanners))
 
 
 if __name__ == '__main__':
