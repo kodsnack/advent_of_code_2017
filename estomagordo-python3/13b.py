fddd9b5482564591d4e40f5f688bbd2383d7d9aa
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-12-13 18:23:02 +0100

    Lucia!

---
+++ estomagordo-python3/13b.py
@@ -0,0 +1,20 @@
+def solve(firewalls):
+    delay = 0
+
+    while True:
+        good = True
+        for depth, rang in firewalls:
+            if (depth + delay) % (2 * (rang - 1)) == 0:
+                good = False
+                break
+
+        if good:
+            break
+        
+        delay += 1
+
+    return delay
+
+with open('input.txt', 'r') as f:
+    firewalls = [list(map(int, line.split(': '))) for line in f.readlines()]
+    print(solve(firewalls))
