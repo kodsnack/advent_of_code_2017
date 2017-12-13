a336e79623e6cb5a840d50b42453f5acf58f4917
torselden <tor.selden@gmail.com>
2017-12-13 22:35:06 +0100

    Day14 stub

---
+++ tor_selden-csharp/tor_selden-csharp/Day14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tor_selden_csharp
+{
+    class Day14
+    {
+        static string[] input = File.ReadAllLines(Path.Combine(Program.BasePath, "input14.txt"));
+
+        internal static void A()
+        {
+            ParseInput();
+
+
+        }
+
+            private static void ParseInput()
+            {
+                foreach (var row in input)
+                {
+                    var temp = row.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+                    
+                }
+            }
+    }
+}
