57a0c75dbec50ce8932f8e1bdd168f879d8ea7f8
Anton Bengtsson <anton@antonbengtsson.net>
2017-12-13 07:10:28 +0100

    Day 13

--- clonest1ck-haskell+python/day13.py
+++ clonest1ck-haskell+python/day13.py
@@ -1,8 +1,22 @@
-def collide(d, r):
-    if(d % (2 * (r - 1)) == 0):
+def collide(t, r):
+    if(r == 0):
+        return False
+    if(t % (2 * (r - 1)) == 0):
         return True
-
     return False
+
+def severity(firewall, time, break_on_sev):
+    severity = 0
+    level = 0
+    while(level < len(firewall)):
+        if(collide(time, firewall[level])):
+            severity += level * firewall[level]
+            if(break_on_sev):
+                return 1
+        level += 1
+        time += 1
+
+    return severity
 
 f = open("in13.txt", 'r')
 f = [x.split("\n")[0].split(": ") for x in f.readlines()]
@@ -16,16 +30,10 @@ for y in range(int(f[-1][0]) + 1):
     if(len(firewall) != y + 1):
         firewall.append(0)
 
-print firewall
+print "Task 1: " + str(severity(firewall, 0, False))
 
-severity = 0
-depth = -1
+delay = 0
+while(severity(firewall, delay, True) != 0):
+    delay += 1
 
-while(depth < len(firewall) - 1):
-    depth += 1
-
-    if(collide(depth, firewall[depth])):
-        print "Depth: " + str(depth) + ", f: " + str(firewall[depth])
-        severity += depth * firewall[depth]
-
-print severity
+print "Task 2: " + str(delay)
