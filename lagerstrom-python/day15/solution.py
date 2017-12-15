02f50863ae4790be64d6a956fb958b0c96c35c48
Alexander Lagerström <alexander@timeedit.se>
2017-12-15 08:44:45 +0100

    Adds solution2 for day15

--- lagerstrom-python/day15/solution.py
+++ lagerstrom-python/day15/solution.py
@@ -3,30 +3,35 @@
 def generator1():
     last_num = 116
     my_factor = 16807
-    for x in range(0, 40000000):
-        remainder = (last_num * my_factor) % 2147483647
-        last_num = remainder
-        yield remainder
+    for x in range(0, 5000000):
+        while True:
+            remainder = (last_num * my_factor) % 2147483647
+            last_num = remainder
+            if remainder % 4 == 0:
+                yield remainder
+                break
 
 def generator2():
     last_num = 299
     my_factor = 48271
-    for x in range(0, 40000000):
-        remainder = (last_num * my_factor) % 2147483647
-        last_num = remainder
-        yield remainder
+    for x in range(0, 5000000):
+        while True:
+            remainder = (last_num * my_factor) % 2147483647
+            last_num = remainder
+            if remainder % 8 == 0:
+                yield remainder
+                break
 
 
 def main():
     judge_count = 0
     for gen1, gen2 in zip(generator1(), generator2()):
+        #if gen1 == None or gen2 == None:
+        #    continue
         last_16_bits_gen1 = '{:032b}'.format(gen1)[-16:]
         last_16_bits_gen2 = '{:032b}'.format(gen2)[-16:]
         if last_16_bits_gen1 == last_16_bits_gen2:
             judge_count += 1
-        #print(last_16_bits_gen1)
-        #print(last_16_bits_gen2)
-        #print('')
     print(judge_count)
 
 if __name__ == '__main__':
