b3398b2aa26f9b12970b71f0e08338255d1b5269
Anton Bengtsson <anton@antonbengtsson.net>
2017-12-16 11:44:12 +0100

    Day 16 and cleanup day 14

--- clonest1ck-haskell+python/day16.py
+++ clonest1ck-haskell+python/day16.py
@@ -11,22 +11,36 @@ def dance(programs, prog_length, moves):
                 [x, y] = [int(x) for x in move]
             else:
                 [x, y] = [programs.index(x) for x in move]
+            programs = [s for s in programs]
             [programs[x], programs[y]] = [programs[y], programs[x]]
+            programs = "".join(programs)
     return programs
 
 def main():
     f = open("in16.txt", 'r')
     moves = f.readline().split("\n")[0].split(",")
-    programs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+    prev_rounds = {}
+    programs = "abcdefghijklmnop"
+    previous = programs
     prog_length = len(programs)
 
-    programs = dance(programs, prog_length, moves)
-    print "Task 1: " + "".join(programs)
-    for i in range(10**9 - 1):
-	if(i % 10000 == 0):
-		print i;
-        programs = dance(programs, prog_length, moves)
-    print "Task 2: " + "".join(programs)
+    times = 0
+    while(times < 10**9):
+        if(times == 1):
+            print "Task 1: " + previous
+        if(prev_rounds.has_key(previous)):
+            [previous, times_old] = prev_rounds[previous]
+            len_loop = times - times_old
+            while(times < 10**9):
+                times += len_loop
+            times -= len_loop
+        else:
+            programs = dance(previous, prog_length, moves)
+            prev_rounds[previous] = [programs, times]
+            previous = programs
+        times += 1
+
+    print "Task 2: " + previous
 
 
 main()
