e1dd1f646b0ac9d15e3d60ac33bb8fd056cfaa2a
Emil Lundberg <emil@emlun.se>
2017-12-16 07:03:14 +0100

    Solve day16b

--- emlun-scala/day16.scala
+++ emlun-scala/day16.scala
@@ -28,6 +28,83 @@ object Day16 extends App {
     case Partner(a, b) => dance(programs, Exchange(programs.indexOf(a), programs.indexOf(b)))
   }
 
+  def danceAll(programs: String, moves: List[Move]): String = moves.foldLeft(programs)(dance)
+
+  def danceTimes(programs: String, moves: List[Move], timesLeft: Int, history: List[String]): String =
+    if (timesLeft > 0) {
+      val next = danceAll(programs, moves)
+      val index = history.indexOf(programs)
+      if (index >= 0) {
+        val period = index + 1
+        if (index == 0) {
+          programs
+        } else if (timesLeft >= period) {
+          println(s"timesLeft: ${timesLeft}, programs: ${programs}, index: ${index}, period: ${period}, history(index): ${history(index)}, history: ${history}")
+          danceTimes(programs, moves, timesLeft % period, history)
+        } else {
+          danceTimes(next, moves, timesLeft - 1, programs +: history)
+        }
+      }
+      else {
+        danceTimes(next, moves, timesLeft - 1, programs +: history)
+      }
+    } else
+      programs
+
+  val startPos = "abcdefghijklmnop"
+  // val startPos = "abcde"
+
   println(moves)
-  println(moves.foldLeft("abcdefghijklmnop")(dance))
+  println(moves.foldLeft(startPos)(dance))
+
+  def stream = Iterator.iterate(startPos)(danceAll(_, moves))
+
+  println(1)
+  println(stream.drop(1).next())
+  println(danceTimes(startPos, moves, 1, Nil))
+
+  println(2)
+  println(stream.drop(2).next())
+  println(danceTimes(startPos, moves, 2, Nil))
+
+  println(29)
+  println(stream.drop(29).next())
+  println(danceTimes(startPos, moves, 29, Nil))
+
+  println(30)
+  println(stream.drop(30).next())
+  println(danceTimes(startPos, moves, 30, Nil))
+
+  println()
+  println(31)
+  println(stream.drop(31).next())
+  println(danceTimes(startPos, moves, 31, Nil))
+
+  println()
+  println(57)
+  println(stream.drop(57).next())
+  println(danceTimes(startPos, moves, 57, Nil))
+
+  println()
+  println(58)
+  println(stream.drop(58).next())
+  println(danceTimes(startPos, moves, 58, Nil))
+
+  println()
+  println(59)
+  println(stream.drop(59).next())
+  println(danceTimes(startPos, moves, 59, Nil))
+
+  println()
+  println(60)
+  println(stream.drop(60).next())
+  println(danceTimes(startPos, moves, 60, Nil))
+
+  println()
+  println(1000)
+  println(stream.drop(1000).next())
+  println(danceTimes(startPos, moves, 1000, Nil))
+
+  println(danceTimes(startPos, moves, 1000000000, Nil))
+
 }
