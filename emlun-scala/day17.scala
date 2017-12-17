a1f244cbe376f627dcb1659cbd6066e85c7a5f31
Emil Lundberg <emil@emlun.se>
2017-12-17 06:40:21 +0100

    Solve day17b

--- emlun-scala/day17.scala
+++ emlun-scala/day17.scala
@@ -8,6 +8,7 @@ object Day17 extends App {
       // case 0 => next
       case i => i
     }
+    println(s"next: ${next}\tpos: ${pos}")
     buffer.splitAt(pos + 1) match {
       case (front, back) => (front :+ next) ++ back
     }
@@ -15,11 +16,25 @@ object Day17 extends App {
 
   def solveA(input: Int) = {
     val finalBuffer = (1 to 2017).foldLeft(Vector(0))(iterate(input))
+    println(finalBuffer(1))
     finalBuffer((finalBuffer.indexOf(2017) + 1) % finalBuffer.length)
   }
 
-  // println(s"Example:\n${solveA(3) mkString "\n"}")
-  println(s"A: ${solveA(3)}")
+  def nextPos(stepsize: Int)(currentPos: Int, iteration: Int): Int = {
+    val pos = ((currentPos + stepsize) % iteration) + 1
+    // println(s"iteration: ${iteration}\tpos: ${pos}")
+    if (pos == 1) {
+      println(iteration)
+    }
+    pos
+  }
+
+  def solveB(input: Int) = {
+    (1 to 50000000).foldLeft(0)(nextPos(input))
+  }
+
+  println(s"Example:\n${(1 to 10).scanLeft(Vector(0))(iterate(3)) mkString "\n"}")
+  // println(s"A: ${solveA(3)}")
   println(s"A: ${solveA(input)}")
-  // println(s"B: ${solveB(moves)}")
+  println(s"B: ${solveB(input)}")
 }
