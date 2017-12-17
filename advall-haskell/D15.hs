a34b31faa3424fbe2f020460d8023f21c5765c33
advall <david@advall.se>
2017-12-17 21:36:16 +0100

    solve day 15

--- advall-haskell/D15.hs
+++ advall-haskell/D15.hs
@@ -1,12 +1,32 @@
 module D15 where
 
-parseInput :: String -> String
-parseInput input = undefined
+import Data.List (foldl', scanl')
+import Data.Bits ((.&.))
+import Data.Word
+
+parseInput :: String -> (Integer,Integer)
+parseInput input = (\[a,b] -> (a,b)) $ map (read . last . words) $ lines input
 
 
-solve1 :: String -> String
-solve1 input = "not yet implemented"
+gen :: Integer -> Integer -> [Integer]
+gen prevVal factor = compVal : gen (nextVal) factor
+    where 
+        compVal = prevVal .&. 0xffff
+        nextVal = (prevVal * factor) `mod` 2147483647
+
+solve1 :: String -> Int
+solve1 input = length $ filter (\b -> b) $ take 40000000 comps
+    where 
+        comps         = zipWith (==) genA genB
+        genA          = gen aSeed 16807
+        genB          = gen bSeed 48271
+        (aSeed,bSeed) = parseInput input
 
 
-solve2 :: String -> String
-solve2 input = "not yet implemented"
+solve2 :: String -> Int
+solve2 input = length $ filter (\b -> b) $ take 5000000 comps
+    where 
+        comps         = zipWith (==) genA genB
+        genA          = filter (\x -> (x `mod` 4) == 0) $ gen aSeed 16807
+        genB          = filter (\x -> (x `mod` 8) == 0) $ gen bSeed 48271
+        (aSeed,bSeed) = parseInput input
