e813642f6f3e7fcc47de499250bbc22a305505fa
Johan Andersson <nilsjohanandersson@gmail.com>
2017-12-17 18:11:06 +0100

    Day 17, thinky solution
    DAY17> (time (part2 367))
    Evaluation took:
      0.515 seconds of real time
      0.515625 seconds of total run time (0.515625 user, 0.000000 system)
      100.19% CPU
      2,054,151,454 processor cycles
      0 bytes consed
    
    25674054
    DAY17>

--- 2017/day17.lisp
+++ 2017/day17.lisp
@@ -28,70 +28,15 @@
 (defun part1 (input)
   (cadr (spin (spinlock input) 2017)))
 
-(defun circular-insert-before (buffer value)
-  (declare (optimize speed))
-  (rplacd buffer (cons (car buffer) (cdr buffer)))
-  (rplaca buffer value))
-
-(defun insert (vector value i)
-  (declare (type (vector fixnum) vector)
-           (type fixnum value i)
-           (optimize speed))
-  (loop for j downfrom (vector-push value vector) to (1+ i)
-        do (rotatef (aref vector j) (aref vector (1- j)))
-        finally (return vector)))
-
-(defun make-chunk ()
-  (make-array 1024
-              :element-type 'fixnum
-              :fill-pointer 0))
-
-(defun split (vector i)
+(defun value-after-zero (step-size n)
   (declare (optimize speed)
-           (type (vector fixnum) vector)
-           (type fixnum i))
-  (let ((before (the (vector fixnum) (make-chunk))))
-    (loop for j from 0 below i
-          do (setf (aref before j) (aref vector j)))
-    (loop for j from i below (length vector)
-          for k from 0
-          do (setf (aref vector k) (aref vector j)))
-    (setf (fill-pointer before) i
-          (fill-pointer vector) (- (length vector) i))
-    (values before vector)))
-
-(defun spinlock* (step-size)
-  (declare (optimize speed)
-           (type fixnum step-size))
-  (let ((pos (circular-buffer (make-chunk)))
-        (i 0)
-        (value 0))
-    (declare (type fixnum i value)
-             (type cons pos))
-    (symbol-macrolet ((chunk (the (vector fixnum) (car pos))))
-      (insert chunk value i)
-      (labels ((advance ()
-                 (loop until (<= i (fill-pointer chunk))
-                       do (decf i (fill-pointer chunk))
-                          (setf pos (cdr pos)))))
-        (lambda ()
-          (incf i (1+ step-size))
-          (incf value)
-          (advance)
-          (when (= (fill-pointer chunk) (array-total-size chunk))
-            (circular-insert-before
-             pos (split chunk (truncate (array-total-size chunk) 2)))
-            (advance))
-          (insert chunk value i)
-          (values pos i))))))
-
-(defun number-after (buffer number)
-  (loop for pos = buffer then (cdr pos)
-        for chunk = (car pos)
-        for i = (position number chunk)
-        when i return (or (and (< (1+ i) (length chunk)) (aref chunk (1+ i)))
-                          (aref (cadr pos) 0))
-        until (eq (cdr pos) buffer)))
+           (type fixnum step-size n))
+  (loop with value fixnum = 0
+        for i fixnum from 1
+        for pos fixnum = 0 then (mod (+ pos step-size 1) i)
+        when (zerop pos) do (setf value i)
+        repeat n
+        finally (return value)))
 
 (defun part2 (input)
-  (number-after (spin (spinlock* input) 50000000 ) 0))
+  (value-after-zero input 50000000))
