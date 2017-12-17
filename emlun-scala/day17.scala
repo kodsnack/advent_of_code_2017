a639fc14f524588d620325ef73b6ff05d9892f16
Emil Lundberg <emil@emlun.se>
2017-12-17 06:52:21 +0100

    Clean up day17 solution

--- emlun-scala/day17.scala
+++ emlun-scala/day17.scala
@@ -1,40 +1,29 @@
 object Day17 extends App {
 
-  val input = 344
+  val input: Int = io.Source.stdin.getLines().next().trim.toInt
 
   def iterate(stepsize: Int)(buffer: Vector[Int], next: Int): Vector[Int] = {
     val currentPos = buffer.indexOf(next - 1)
-    val pos = (currentPos + stepsize) % next match {
-      // case 0 => next
-      case i => i
-    }
-    println(s"next: ${next}\tpos: ${pos}")
+    val pos = (currentPos + stepsize) % next
     buffer.splitAt(pos + 1) match {
       case (front, back) => (front :+ next) ++ back
     }
   }
 
-  def solveA(input: Int) = {
-    val finalBuffer = (1 to 2017).foldLeft(Vector(0))(iterate(input))
-    println(finalBuffer(1))
+  def solveA(stepsize: Int) = {
+    val finalBuffer = (1 to 2017).foldLeft(Vector(0))(iterate(stepsize))
     finalBuffer((finalBuffer.indexOf(2017) + 1) % finalBuffer.length)
   }
 
-  def nextPos(stepsize: Int)(currentPos: Int, iteration: Int): Int = {
-    val pos = ((currentPos + stepsize) % iteration) + 1
-    // println(s"iteration: ${iteration}\tpos: ${pos}")
-    if (pos == 1) {
-      println(iteration)
+  def solveB(iterations: Int, stepsize: Int, remembered: Int, currentPos: Int, iteration: Int): Int =
+    if (iteration > iterations)
+      remembered
+    else {
+      val pos = ((currentPos + stepsize) % iteration) + 1
+      solveB(iterations, stepsize, if (pos == 1) iteration else remembered, pos, iteration + 1)
     }
-    pos
-  }
+  def solveB(stepsize: Int): Int = solveB(50000000, stepsize, 0, 0, 1)
 
-  def solveB(input: Int) = {
-    (1 to 50000000).foldLeft(0)(nextPos(input))
-  }
-
-  println(s"Example:\n${(1 to 10).scanLeft(Vector(0))(iterate(3)) mkString "\n"}")
-  // println(s"A: ${solveA(3)}")
   println(s"A: ${solveA(input)}")
   println(s"B: ${solveB(input)}")
 }
