3a1504a9e06e739efcfbf8e85b79ca5741a91aa6
Alexander Lagerström <alexander@timeedit.se>
2017-12-19 21:28:07 +0100

    Updates solutions for day19 to a nicer one

--- lagerstrom-python/day19/solution.py
+++ lagerstrom-python/day19/solution.py
@@ -3,11 +3,11 @@ import string
 
 class Solution():
     def __init__(self):
-        self.seen_chars = {}
-        self.alphabet = list(string.ascii_uppercase)
         self.input_lines = []
         with open('data.txt', 'r') as f:
             self.input_lines = f.read().split('\n')
+        self.seen_chars = {}
+        self.alphabet = list(string.ascii_uppercase)
         self.input_lines = list(filter(None, self.input_lines))
         self.current_token = '|'
         self.y_pos = 0
@@ -21,17 +21,20 @@ class Solution():
     def get_next(self):
         self.y_pos += self.direction_set[0]
         self.x_pos += self.direction_set[1]
-        self.steps += 1
 
         next_char = self.input_lines[self.y_pos][self.x_pos]
         if next_char in self.alphabet:
             self.seen_chars[next_char] = True
-            if next_char == 'T' or next_char == 'F':
-                print('Answer1: %s' % ''.join(self.seen_chars))
-                print('Answer2: %s' % self.steps)
-                exit(1)
+
         if next_char == '+':
             self.find_turn()
+
+        if next_char == ' ':
+            print('Answer1: %s' % ''.join(self.seen_chars.keys()))
+            print('Answer2: %d' % self.steps)
+            exit(1)
+        self.steps += 1
+
 
     def find_turn(self):
         neighbours = (
