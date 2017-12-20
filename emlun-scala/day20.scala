164ad45ea288dcf4889decc2f657946aecabb43e
Emil Lundberg <emil@emlun.se>
2017-12-20 06:48:09 +0100

    Clean up day20 a bit

--- emlun-scala/day20.scala
+++ emlun-scala/day20.scala
@@ -7,9 +7,6 @@ object Day20 extends App {
     def +(b: V3) = (a, b) match {
       case ((aa, ab, ac), (ba, bb, bc)) => (aa + ba, ab + bb, ac + bc)
     }
-    def unary_-(): V3 = a match {
-      case (aa, ab, ac) => (-aa, -ab, -ac)
-    }
     def manhattan: Int = a match {
       case (a, b, c) => Math.abs(a) + Math.abs(b) + Math.abs(c)
     }
@@ -20,12 +17,36 @@ object Day20 extends App {
 
   type V3 = (Int, Int, Int)
   case class Particle(id: Int, p: V3, v: V3, a: V3) {
-    def step: Particle =
-      copy(v = v + a, p = p + (v + a))
+    def step: Particle = copy(v = v + a, p = p + (v + a))
     def willTurn: Boolean = v.signa == a.signa
   }
 
   val particlePattern = raw"p=<\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)>, v=<\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)>, a=<\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)>".r
+
+  def step(particles: List[Particle]): List[Particle] =
+    particles map { _.step }
+
+  def stepB(particles: List[Particle]): List[Particle] =
+    removeColliders(particles map { _.step })
+
+  def findClosest(particles: List[Particle]): Particle =
+    particles minBy { particle => particle.a.manhattan }
+
+  def removeColliders(particles: List[Particle]): List[Particle] =
+    particles
+      .groupBy { _.p }
+      .filter { case (_, group) => group.size == 1 }
+      .flatMap { case (_, group) => group }
+      .toList
+
+  def solveA(particles: List[Particle]) =
+    findClosest(particles).id
+
+  def solveB(particles: List[Particle]) =
+    Iterator.iterate(particles)((step _) andThen removeColliders)
+      .drop(10000)
+      .next()
+      .size
 
   val particles: List[Particle] = (
     io.Source.stdin.getLines()
@@ -42,36 +63,8 @@ object Day20 extends App {
             )
         }
       }
-  ).toList
-
-  def step(particles: List[Particle]): List[Particle] =
-    particles map { _.step }
-
-  def stepB(particles: List[Particle]): List[Particle] =
-    removeColliders(particles map { _.step })
-
-  def findClosest(particles: List[Particle]): Particle = {
-    particles
-      .minBy { particle => particle.a.manhattan }
-  }
-
-  def removeColliders(particles: List[Particle]): List[Particle] =
-    particles
-      .groupBy { _.p }
-      .filter { case (_, group) => group.size == 1 }
-      .flatMap { case (_, group) => group }
       .toList
-
-  def solveA(particles: List[Particle]) = {
-    // val states = Iterator.iterate(particles)(step)
-    // (states map findClosest).take(10).toList
-    findClosest(particles).id
-  }
-
-  def solveB(particles: List[Particle]) = {
-    val states = Iterator.iterate(particles)(stepB)
-    (states).drop(100000).next().size
-  }
+  )
 
   println(s"A: ${solveA(particles)}")
   println(s"B: ${solveB(particles)}")
