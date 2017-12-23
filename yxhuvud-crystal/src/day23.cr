33ffb977bc757c49a29916c89d391ae9f33848e6
Linus Sellberg <sellberg@gmail.com>
2017-12-23 09:56:11 +0100

    Day23: Cleanup and deoptimization.
    Not parsing the program anymore. It is 8 times slower, but the
    code is so much more readable. 0.3s is fast enough.

--- yxhuvud-crystal/src/day23.cr
+++ yxhuvud-crystal/src/day23.cr
@@ -1,44 +1,16 @@
 program = File.read(ARGV[0]).lines
 
 class Machine
-  property id : Int32
   property regs : Hash(String, Int64)
   property pos
-  property queue
-  property blocked
-  property other : Nil | self
-  property sent
   property invoked
-  property program : Array(Instruction)
+  property program : Array(String)
 
-  def initialize(@id, program)
+  def initialize(@program)
     @regs = registry
-    regs["p"] = @id.to_i64
     @pos = 0
-    @queue = [] of Int64
-    @blocked = false
-    @other = nil
-    @sent = 0
     @invoked = 0
-    @program = parser(program)
   end
-
-  def parser(program)
-    program.map do |l|
-      parse(l, [Snd, Rcv, Set, Add, Sub, Mul, Mod, Jgz, Jnz])
-    end
-  end
-
-  macro parse(line, ops)
-      case l
-      {% for name in ops %}
-       when {{name}}::PATTERN
-         {{name}}.new $1, $2
-      {% end %}
-      else
-         raise "unhandled: #{{{line}}}"
-      end
-    end
 
   def registry
     Hash(String, Int64).new do |h, k|
@@ -51,111 +23,32 @@ class Machine
   end
 
   def run
-    until blocked || pos == program.size
-      if program[pos].call(self)
-        next
+    until pos == program.size
+      case l = program[pos]
+      when /set (.) ([-\w]+)/
+        regs[$1] = regs[$2]
+      when /add (.) ([-\w]+)/
+        regs[$1] += regs[$2]
+      when /sub (.) ([-\w]+)/
+        regs[$1] -= regs[$2]
+      when /mul (.) ([-\w]+)/
+        @invoked += 1
+        regs[$1] *= regs[$2]
+      when /mod (.) ([-\w]+)/
+        regs[$1] %= regs[$2]
+      when /jgz (.) ([-\w]+)/
+        @pos += regs[$2] - 1 if regs[$1] > 0
+      when /jnz (.) ([-\w]+)/
+        @pos += regs[$2] - 1 if regs[$1] != 0
       else
-        @pos += 1
+        raise "unhandled: #{l}"
       end
-    end
-  end
-
-  abstract struct Instruction
-    property arg1 : String
-    property arg2 : String
-
-    def initialize(@arg1, @arg2); end
-  end
-
-  struct Snd < Instruction
-    PATTERN = /snd ([-\w]+)/
-
-    def call(machine)
-      if o = machine.other
-        o.queue << machine.regs[arg1]
-        o.blocked = false
-        machine.sent += 1
-      end
-      false
-    end
-  end
-
-  struct Rcv < Instruction
-    PATTERN = /rcv ([-\w]+)/
-
-    def call(machine)
-      return true if machine.blocked = machine.queue.empty?
-      machine.regs[arg1] = machine.queue.shift.not_nil!
-      false
-    end
-  end
-
-  struct Set < Instruction
-    PATTERN = /set (.) ([-\w]+)/
-
-    def call(machine)
-      machine.regs[@arg1] = machine.regs[@arg2]
-      false
-    end
-  end
-
-  struct Add < Instruction
-    PATTERN = /add (.) ([-\w]+)/
-
-    def call(machine)
-      machine.regs[@arg1] += machine.regs[@arg2]
-      false
-    end
-  end
-
-  struct Sub < Instruction
-    PATTERN = /sub (.) ([-\w]+)/
-
-    def call(machine)
-      machine.regs[@arg1] -= machine.regs[@arg2]
-      false
-    end
-  end
-
-  struct Mul < Instruction
-    PATTERN = /mul (.) ([-\w]+)/
-
-    def call(machine)
-      machine.invoked += 1
-      machine.regs[@arg1] *= machine.regs[@arg2]
-      false
-    end
-  end
-
-  struct Mod < Instruction
-    PATTERN = /mod (.) ([-\w]+)/
-
-    def call(machine)
-      machine.regs[@arg1] %= machine.regs[@arg2]
-      false
-    end
-  end
-
-  struct Jgz < Instruction
-    PATTERN = /jgz (.) ([-\w]+)/
-
-    def call(machine)
-      machine.pos += machine.regs[@arg2] - 1 if machine.regs[@arg1] > 0
-      false
-    end
-  end
-
-  struct Jnz < Instruction
-    PATTERN = /jnz (.) ([-\w]+)/
-
-    def call(machine)
-      machine.pos += machine.regs[@arg2] - 1 if machine.regs[@arg1] != 0
-      false
+      @pos += 1
     end
   end
 end
 
-prog = Machine.new(0, program)
+prog = Machine.new(program)
 prog.run
 puts "part1: #{prog.invoked}"
 
@@ -175,8 +68,8 @@ jgz g 2
 jgz 1 -13
 EOS
 
-prog = Machine.new(0, program)
-prog.program[11..23] = prog.parser(replacement.strip.lines)
+prog = Machine.new(program)
+prog.program[11..23] = replacement.strip.lines
 prog.regs["a"] = 1_i64
 prog.run
 puts "part2: #{prog.regs["h"]}"
