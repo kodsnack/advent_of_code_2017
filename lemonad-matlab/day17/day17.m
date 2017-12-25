4f94d282434e64e783e3e153135d2cacf2caf336
Jonas Nockert <jonasnockert@gmail.com>
2017-12-26 00:40:36 +0100

    Cleanup day 17

--- lemonad-matlab/day17/day17.m
+++ lemonad-matlab/day17/day17.m
@@ -1,6 +1,18 @@
 %
 % Day 17, Advent of code 2017 (Jonas Nockert / @lemonad)
 %
+% TODO Find way of solving problem 1 like problem 2.
+%
+
+steps = 382;
+val= solvePartOne(steps);
+fprintf('Value after 2017: %d\n', val);
+assert(val == 1561)
+
+val = solvePartTwo(steps);
+fprintf('Value after 0: %d\n', val);
+assert(val == 33454823)
+return
 
 steps = 3;
 % steps = 382;
@@ -28,64 +40,58 @@ before
 after
 disp('end')
 
-return
 
+function after_zero = solvePartTwo(steps)
+    N = 50000000;
 
+    buffer_len = 1;  % Buffer contains only element 0 from start.
+    index = 1;
+    zero_index = 0;
+    after_zero = 0;
 
-
-N = 50000000;
-
-buffer_len = 1;  % Buffer contains only element 0 from start.
-index = 1;
-zero_index = 0;
-after_zero = 0;
-
-% Note: Since we're not actually creating a buffer, use
-% 0-based indexing.
-for i = 0:N-1
-    index = mod(index + steps + 1, buffer_len);
-        zero_index = zero_index + 1;
-    if index == zero_index
-        % New number is inserted immediately after the
-        % zero element.
-        after_zero = i + 1;
+    % Note: Since we're not actually creating a buffer, use
+    % 0-based indexing.
+    for i = 0:N-1
+        index = mod(index + steps + 1, buffer_len);
+        if index < zero_index
+            zero_index = zero_index + 1;
+        elseif index == zero_index
+            % New number is inserted immediately after the
+            % zero element.
+            after_zero = i + 1;
+        end
+        buffer_len = buffer_len + 1;
     end
-    buffer_len = buffer_len + 1;
 end
 
-disp(after_zero)
 
-return
+function val = solvePartOne(steps)
+    buffer = [0];
+    index = 0;
 
-
-
-buffer = [0];
-index = 0;
-
-for i = 1:2017
-    [buffer, index] = stepForward(buffer, index, steps);
-end
-
-index_2017 = find(buffer == 2017);
-buffer(index_2017 + 1)
-
-
-
-buffer = [0];
-index = 0;
-
-for i = 1:50000000
-    if mod(i, 100000) == 0
-        fprintf('i = %d\n', i);
+    for i = 1:2017
+        [buffer, index] = stepForward(buffer, index, steps);
     end
-    [buffer, index] = stepForward(buffer, index, steps);
+
+    index_2017 = find(buffer == 2017);
+    val = buffer(index_2017 + 1)
 end
 
-index_0 = find(buffer == 0)
-buffer(index_2017 + 1)
 
+function val = solvePartTwo2(steps)
+    buffer = [0];
+    index = 0;
 
-return
+    for i = 1:50000000
+        if mod(i, 100000) == 0
+            fprintf('i = %d\n', i);
+        end
+        [buffer, index] = stepForward(buffer, index, steps);
+    end
+
+    index_0 = find(buffer == 0);sssssssssssssssssssssssssssssssswwwwww
+    buffer(index_2017 + 1)
+end
 
 
 function [buffer, index] = stepForward(buffer, index, steps)
